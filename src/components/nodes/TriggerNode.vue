27220b6d0bc3dc6dc280781aac9e8a4965b9d38b
teukurian <teukurian090598@gmail.com>
2025-12-07 10:53:18 +0800

    Test with shadcn

--- src/components/nodes/TriggerNode.vue
+++ src/components/nodes/TriggerNode.vue
@@ -1,23 +1,18 @@
 <template>
-  <div class="node trigger-node">
-    <h4>Trigger</h4>
-    <p>Type: {{ data.type }}</p>
-    <p>Once per contact: {{ data.oncePerContact }}</p>
-  </div>
+  <Card>
+    <CardHeader class="border border-black"> <Zap /> Trigger </CardHeader>
+    <CardContent>
+      <p>Conversation Opened</p>
+    </CardContent>
+  </Card>
 </template>
 
 <script setup>
+import { Card, CardHeader, CardContent } from "../ui/card";
+import { Zap } from "lucide-vue-next";
+
 defineProps({
   id: String,
-  data: Object
-})
+  data: Object,
+});
 </script>
-
-<style scoped>
-.trigger-node {
-  padding: 12px;
-  background: #fff8e6;
-  border: 1px solid #e0c785;
-  border-radius: 6px;
-}
-</style>
